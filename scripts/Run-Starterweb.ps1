3a2f15d43a474c13cb52f94bcf194dfc4e26f4f6
Ryan Brandenburg <rybrande@microsoft.com>
2018-06-28 16:23:42 -0700

    Use bootstrap4 for templates

---
+++ scripts/Run-Starterweb.ps1
@@ -0,0 +1,29 @@
+#!/usr/bin/env powershell
+#requires -version 4
+
+[CmdletBinding(PositionalBinding = $false)]
+param()
+
+Set-StrictMode -Version 2
+$ErrorActionPreference = 'Stop'
+
+./build.cmd /t:Package
+
+Push-Location "$PSScriptRoot/../src/Microsoft.DotNet.Web.ProjectTemplates/content/StarterWeb-CSharp"
+try {
+    $csproj = "Company.WebApplication1.csproj"
+    (Get-Content $csproj).replace('netcoreapp2.2', 'netcoreapp2.1') | Set-Content $csproj
+    
+    $sqlServer = "Data\SqlServer"
+    if (Test-Path $sqlServer) {
+        Remove-Item -Recurse -Force -Path $sqlServer
+    }
+
+    $launchSettings = "Properties\launchSettings.json"
+    (Get-Content $launchSettings).replace('"sslPort": 0', '') | Set-Content $launchSettings 
+
+    dotnet run
+}
+finally {
+    Pop-Location
+}
